4b7224d7a638de18e729cdea198e21a429546ef3
Nohovskyi <nogovskiy@gmail.com>
2023-03-21 09:11:36 +0200

    Додає зміни в шляхи

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,8 @@ export const App = () => {
         <Route index element={<Home title="Trending week" />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="/movies/:movieId/cast" element={<Cast />} />
-          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
         <Route path="*" element={<Header />} />
       </Route>
